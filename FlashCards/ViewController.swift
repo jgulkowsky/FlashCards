b1c7ee97a4f4d10de52f3748a8889d80e656ddaa
user <user@users-macbook-pro.local>
2018-12-18 17:31:13 +0100

    Initial Commit

---
+++ FlashCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashCards
+//
+//  Created by user on 18/12/2018.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
